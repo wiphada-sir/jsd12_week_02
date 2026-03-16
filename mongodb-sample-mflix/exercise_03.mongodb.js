a39f1a324673b2e64d963c2e49b94c51248e8e7f
wiphada-sir <wiphada.sir@gmail.com>
2026-03-16 17:02:41 +0700

    added exercise_03.mongodb.js

---
+++ mongodb-sample-mflix/exercise_03.mongodb.js
@@ -0,0 +1,17 @@
+use("sample_mflix");
+
+db.theaters.find({
+    "location.address.state": "AL"
+}) .count();
+
+db.theaters.find({
+    "location.address.city": "La Quinta"
+}) .count();
+
+db.theaters.findOne({
+    "location.address.state": "AL"
+});
+
+db.theaters.findOne({
+    "location.address.city": "La Quinta"
+});
