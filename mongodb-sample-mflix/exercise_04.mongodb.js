b7896b628a300b94e72e0a9a86c6456c253bee40
wiphada-sir <wiphada.sir@gmail.com>
2026-03-16 17:05:28 +0700

    added exercise_04.mongodb.js

---
+++ mongodb-sample-mflix/exercise_04.mongodb.js
@@ -0,0 +1,17 @@
+use("sample_mflix");
+
+db.movies.find({
+    plot: { $regex: "American", $options: "i" }
+}) .count();
+
+db.movies.find({
+    plot: { $regex: "street.$", $options: "i" }
+}) .count();
+
+db.movies.find({
+    plot: { $regex: "American", $options: "i" }
+});
+
+db.movies.find({
+    plot: { $regex: "street.$", $options: "i" }
+});
